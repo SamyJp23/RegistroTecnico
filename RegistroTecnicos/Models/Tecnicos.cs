37c4ff80489f58b745ef2aa274784542ae078019
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-05-08 03:58:51 -0400

    Creando el modelo

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegistroTecnicos.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int IdTecnico { get; set; }
+        [Required(ErrorMessage = "El campo nombre es obligatorio")]
+        public string? Nombre { get; set; }
+
+        public double SueldoHora { get; set; }
+    }
+}
