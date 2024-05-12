789cbbda0ae1462382f028e2fd8b0496ecc90392
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-05-12 18:13:04 -0400

    Agregando la libreria de Boostrap al proyecto

--- RegistroTecnicos/Program.cs
+++ RegistroTecnicos/Program.cs
@@ -8,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddSingleton<Radzen.NotificationService>();
+builder.Services.AddBlazorBootstrap();
+
 
 //Obtener el constructor
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
