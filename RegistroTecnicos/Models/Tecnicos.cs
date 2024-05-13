3ef4f77e6e2f12e796c3ffee92342cde922723d2
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-05-13 01:00:49 -0400

    Agregando las Validaciones que me faltaban lasa cuales eran la del Id y el sueldo por hora.

--- RegistroTecnicos/Models/Tecnicos.cs
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -4,10 +4,11 @@ namespace RegistroTecnicos.Models
     public class Tecnicos
     {
         [Key]
+        [Range(1, int.MaxValue, ErrorMessage = "El ID debe ser mayor o igual que 1.")]
         public int IdTecnico { get; set; }
-        [Required(ErrorMessage = "El campo nombre es obligatorio")]
+        [Required(ErrorMessage = "El campo nombre es obligatorio.")]
         public string? Nombre { get; set; }
-
+        [Range(0.01, double.MaxValue, ErrorMessage = "El campo sueldo por hora debe ser mayor que cero.")]
         public double SueldoHora { get; set; }
     }
 }
