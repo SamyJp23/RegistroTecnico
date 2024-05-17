a0c7a30f553958f80e9921709fcb6c7954ee5b20
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-05-16 23:45:46 -0400

    Actualizando la base de datos con un nuevo campo llamado TipoTecnico

---
+++ RegistroTecnicos/Migrations/20240517034207_TiposTecnicos.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class TiposTecnicos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TipoTecnico",
+                table: "Tecnicos",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TipoTecnico",
+                table: "Tecnicos");
+        }
+    }
+}
