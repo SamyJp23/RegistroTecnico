3a7bfe29a634551e9016c68aa69e6812df919c7c
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-05-17 23:45:10 -0400

    Creando el modelo TiposTecnicos

---
+++ RegistroTecnicos/Models/TiposTecnicos.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace RegistroTecnicos.Models
+{
+    public class TiposTecnicos
+    {
+
+        [Key]
+        [Range(1, int.MaxValue, ErrorMessage = "El ID debe ser mayor o igual que 1.")]
+        public int TipoTecnicoId { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
+        public string Descripcion { get; set; }
+        public ICollection<Tecnicos> Tecnicos { get; set; }
+    }
+}
