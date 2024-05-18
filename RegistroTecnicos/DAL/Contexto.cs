631f4a9238e8ae0de2e5e83e6556f871dacc77b8
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-05-17 23:41:35 -0400

    configurando el contexto para establecer la conexion entre tecnicos y tipos tecnicos en la base de datos

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -5,9 +5,24 @@ namespace RegistroTecnicos.DAL
     public class Contexto : DbContext
     {
 
-        public Contexto(DbContextOptions<Contexto> options) 
-               : base(options) { }
+       
 
-        public DbSet<Tecnicos> Tecnicos { get; set;}
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options)
+        {
+        }
+        public DbSet<Tecnicos> Tecnicos { get; set; }
+        public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Tecnicos>()
+                .HasOne(tt => tt.TipoTecnico)
+                .WithMany(t => t.Tecnicos)
+                .HasForeignKey(t => t.TipoTecnicoId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
