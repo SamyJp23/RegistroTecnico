a38d179cf79876c668a88eb30179235cbc13dbfc
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-05-21 00:48:21 -0400

    Eliminando los  { } del namespace y agregando ; para ponerlo a nivel de archivo

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -1,28 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.Models;
-namespace RegistroTecnicos.DAL
+namespace RegistroTecnicos.DAL;
+
+public class Contexto : DbContext
 {
-    public class Contexto : DbContext
+
+   
+
+    public Contexto(DbContextOptions<Contexto> options)
+        : base(options)
     {
-
-       
-
-        public Contexto(DbContextOptions<Contexto> options)
-            : base(options)
-        {
-        }
-        public DbSet<Tecnicos> Tecnicos { get; set; }
-        public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            
-            modelBuilder.Entity<Tecnicos>()
-                .HasOne(tt => tt.TipoTecnico)
-                .WithMany(t => t.Tecnicos)
-                .HasForeignKey(t => t.TipoTecnicoId);
-
-            base.OnModelCreating(modelBuilder);
-        }
-
     }
+    public DbSet<Tecnicos> Tecnicos { get; set; }
+    public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+        modelBuilder.Entity<Tecnicos>()
+            .HasOne(tt => tt.TipoTecnico)
+            .WithMany(t => t.Tecnicos)
+            .HasForeignKey(t => t.TipoTecnicoId);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
 }
