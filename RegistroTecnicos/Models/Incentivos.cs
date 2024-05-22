63812e70b6c000bedac9a23ca44e8b411a57d9f7
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-05-21 23:26:03 -0400

    Creando el modelo Incentivos

---
+++ RegistroTecnicos/Models/Incentivos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+    public class Incentivos
+    {
+    [Key]
+    public int IncentivoId { get; set; }
+
+    public string Descripcion { get; set; }
+    public int CantidadIncentivos { get; set; }
+    public int Monto { get; set; }
+
+    public int TecnicoId { get; set; }
+    }
+
