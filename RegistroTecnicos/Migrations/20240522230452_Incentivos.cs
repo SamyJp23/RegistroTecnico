c66f9233cf781eacf12ddbf31179e1e511949fa7
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-05-22 19:06:31 -0400

    Creando nueva Migracion

---
+++ RegistroTecnicos/Migrations/20240522230452_Incentivos.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Incentivos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Incentivo",
+                table: "TiposTecnicos",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "Incentivos",
+                columns: table => new
+                {
+                    IncentivoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    CantidadServicios = table.Column<int>(type: "INTEGER", nullable: false),
+                    Monto = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TecnicoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incentivos", x => x.IncentivoId);
+                    table.ForeignKey(
+                        name: "FK_Incentivos_Tecnicos_TecnicoId",
+                        column: x => x.TecnicoId,
+                        principalTable: "Tecnicos",
+                        principalColumn: "IdTecnico",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incentivos_TecnicoId",
+                table: "Incentivos",
+                column: "TecnicoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Incentivos");
+
+            migrationBuilder.DropColumn(
+                name: "Incentivo",
+                table: "TiposTecnicos");
+        }
+    }
+}
