57846768cd525aad7b57b07ddd995b20d7936323
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-05-22 00:52:35 -0400

    Agregando la anotaciones, creando la propiedad Fecha que me falto y cambiando el nombre de la propiedad CantidadIncentivos a CantidadServicios

--- RegistroTecnicos/Models/Incentivos.cs
+++ RegistroTecnicos/Models/Incentivos.cs
@@ -5,11 +5,16 @@ namespace RegistroTecnicos.Models;
     public class Incentivos
     {
     [Key]
+    [Range(1, int.MaxValue, ErrorMessage = "El ID debe ser mayor o igual que 1.")]
     public int IncentivoId { get; set; }
-
+    [Required(ErrorMessage = "El campo descripcion es obligatorio.")]
     public string Descripcion { get; set; }
-    public int CantidadIncentivos { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "La Cantidad de servicios debe ser mayor a 0.")]
+    public int CantidadServicios { get; set; }
+    [Required(ErrorMessage = "El campo Monto es obligatorio.")]
     public int Monto { get; set; }
+
+    public DateTime Fecha { get; set; }
 
     public int TecnicoId { get; set; }
     }
