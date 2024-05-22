d24cc794745b88de1f18dd99fdda160e78d3eba9
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-05-22 00:27:43 -0400

    Decalrando la propiedad DbSet<Incentivos>

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -5,14 +5,13 @@ namespace RegistroTecnicos.DAL;
 public class Contexto : DbContext
 {
 
-   
-
     public Contexto(DbContextOptions<Contexto> options)
         : base(options)
     {
     }
     public DbSet<Tecnicos> Tecnicos { get; set; }
     public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+    public DbSet<Incentivos> Incentivos { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         
